54c1cea5b765b0d59f180a12b4c3935c285f9be8
Simon Basset <simon.basset@gmail.com>
2023-01-29 13:30:48 +0100

    feat: using Button from kobalte

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,11 +1,12 @@
 import { createSignal } from "solid-js";
 import "./Counter.css";
+import { Button } from "@kobalte/core";
 
 export default function Counter() {
   const [count, setCount] = createSignal(0);
   return (
-    <button class="increment" onClick={() => setCount(count() + 1)}>
+    <Button.Root class="increment" onClick={() => setCount(count() + 1)}>
       Clicks: {count()}
-    </button>
+    </Button.Root>
   );
 }
